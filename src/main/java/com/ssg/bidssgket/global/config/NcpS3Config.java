beb5ec5c45f945cc2dfecf143e6cb12c011edd1f
OOOIOOOIO <polite159@gmail.com>
2024-07-31 11:49:54 +0900

    NCP S3 config

---
+++ src/main/java/com/ssg/bidssgket/global/config/NcpS3Config.java
@@ -0,0 +1,42 @@
+package com.ssg.bidssgket.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class NcpS3Config {
+
+    @Value("${ncp.s3.end-point}")
+    private String endPoint;
+    @Value("${ncp.s3.region-name}")
+    private String regionName;
+    @Value("${ncp.s3.bucket-name}")
+    private String bucketName;
+    @Value("${ncp.s3.access-key}")
+    private String accessKey;
+    @Value("${ncp.s3.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3 s3() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+
+
+}
+
