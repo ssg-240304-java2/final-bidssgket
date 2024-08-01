9b25cc48ba0b4227dd8ac66be6c28d1bf4aad19c
Gyunorld <mingue640@gmail.com>
2024-08-01 17:42:23 +0900

    index page

--- src/main/java/com/ssg/bidssgket/user/domain/product/view/ProductViewController.java
+++ src/main/java/com/ssg/bidssgket/user/domain/product/view/ProductViewController.java
@@ -1,4 +1,12 @@
 package com.ssg.bidssgket.user.domain.product.view;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductViewController {
+    @GetMapping("")
+    public String indexController() {
+        return "index";
+    }
 }
