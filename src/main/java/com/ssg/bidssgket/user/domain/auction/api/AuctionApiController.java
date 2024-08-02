150f4ab767ce060bec10f4e3d6c3eded73b9474c
yujin4sth00 <yujin4sth00@gmail.com>
2024-08-02 09:54:30 +0900

    user 도메인에 경매 폴더 추가

---
+++ src/main/java/com/ssg/bidssgket/user/domain/auction/api/AuctionApiController.java
@@ -0,0 +1,4 @@
+package com.ssg.bidssgket.user.domain.auction.api;
+
+public class AuctionApiController {
+}
--- src/main/java/com/ssg/bidssgket/user/domain/payment/domain/Payment.java
+++
@@ -1,4 +0,0 @@
-package com.ssg.bidssgket.user.domain.payment.domain;
-
-public class Payment {
-}
