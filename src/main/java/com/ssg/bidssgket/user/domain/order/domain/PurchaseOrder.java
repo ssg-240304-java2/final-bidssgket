0f1edadb1bb700495c2f0b4669ba116055d013db
finite2030 <finite2030@gmail.com>
2024-08-02 21:54:14 +0900

    feat: order & pay Entity Updated

--- src/main/java/com/ssg/bidssgket/user/domain/order/domain/PurchaseOrder.java
+++ src/main/java/com/ssg/bidssgket/user/domain/order/domain/PurchaseOrder.java
@@ -1,10 +1,12 @@
 package com.ssg.bidssgket.user.domain.order.domain;
 
 import com.ssg.bidssgket.common.domain.BaseTimeAndDeleteEntity;
+import com.ssg.bidssgket.user.domain.member.domain.Member;
 import com.ssg.bidssgket.user.domain.order.domain.enums.DeliveryType;
 import com.ssg.bidssgket.user.domain.order.domain.enums.OrderStatus;
 import com.ssg.bidssgket.user.domain.order.domain.enums.TransactionType;
 import com.ssg.bidssgket.user.domain.payment.domain.Payment;
+import com.ssg.bidssgket.user.domain.product.domain.Product;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -15,7 +17,7 @@ import lombok.*;
 public class PurchaseOrder extends BaseTimeAndDeleteEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int purchaseOrderNo; // 판매 주문 번호 [PK]
+    private int purchaseOrderNo; // 구매 주문 번호 [PK]
 
     @Enumerated(EnumType.STRING)
     private TransactionType transactionType; // 경매, 즉시구매
@@ -27,32 +29,36 @@ public class PurchaseOrder extends BaseTimeAndDeleteEntity {
     private OrderStatus orderStatus; // 결제대기, 결제완료, 배송대기, 배송중, 배송완료, 주문완료, 결제취소, 주문취소
 
     @OneToOne
-    @JoinColumn(name = "paymentNo")
+    @JoinColumn(name = "payment_no")
     private Payment payment; // 결제 번호 [FK]
 
+    @ManyToOne
+    @JoinColumn(name = "buyer_no")
+    private Member buyer; // 구매자 회원 번호 [FK]
+
+    @OneToOne
+    @JoinColumn(name = "product_no")
+    private Product product; // 상품 번호 [FK]
+
     @Builder
-    private PurchaseOrder(TransactionType transactionType, DeliveryType deliveryType, OrderStatus orderStatus, Payment payment) {
+    private PurchaseOrder(TransactionType transactionType, DeliveryType deliveryType, OrderStatus orderStatus, Payment payment, Member buyer, Product product) {
         this.transactionType = transactionType;
         this.deliveryType = deliveryType;
         this.orderStatus = orderStatus;
         this.payment = payment;
+        this.buyer = buyer;
+        this.product = product;
     }
 
-    public static PurchaseOrder createPurchaseOrder(TransactionType transactionType, DeliveryType deliveryType, OrderStatus orderStatus, Payment payment) {
+    public static PurchaseOrder createPurchaseOrder(TransactionType transactionType, DeliveryType deliveryType, OrderStatus orderStatus, Payment payment, Member buyer, Product product) {
 
         return PurchaseOrder.builder()
                 .transactionType(transactionType)
                 .deliveryType(deliveryType)
                 .orderStatus(orderStatus)
                 .payment(payment)
+                .buyer(buyer)
+                .product(product)
                 .build();
     }
-
-//    @OneToOne
-//    @JoinColumn(name = "memberNo")
-//    private Member member;
-//
-//    @OneToOne
-//    @JoinColumn(name = "productNo")
-//    private Product product;
 }
