d00d243ebfaa9d6b2eb317928a4a61919f1c5289
OOOIOOOIO <polite159@gmail.com>
2024-08-09 15:19:50 +0900

    feat: payment 목록 조회 개발

--- src/main/java/com/ssg/bidssgket/admin/api/payment/controller/AdminPaymentApiController.java
+++ src/main/java/com/ssg/bidssgket/admin/api/payment/controller/AdminPaymentApiController.java
@@ -1,12 +1,15 @@
-package com.ssg.bidssgket.admin.api.payment;
+package com.ssg.bidssgket.admin.api.payment.controller;
 
 
+import com.ssg.bidssgket.admin.api.payment.application.AdminPaymentApiService;
+import com.ssg.bidssgket.admin.api.payment.controller.dto.res.AdminPaymentResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,20 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdminPaymentApiController {
 
+    private final AdminPaymentApiService adminPaymentApiService;
 
     /**
      * 결제 목록 조회
      * paging 처리(ajax)
      */
     @GetMapping("/list")
-    public ResponseEntity<Page<String>> getUserList(@PageableDefault(size = 32, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable){
+    public ResponseEntity<Page<AdminPaymentResDto>> getPaymentList(@PageableDefault(size = 32, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable){
 
         log.info("==== get payment List ====");
 
-//        Page<String> carList = carInfoService.getCarList(pageable);
+        Page<AdminPaymentResDto> paymentList = adminPaymentApiService.getPaymentList(pageable);
 
-//        return new ResponseEntity<>(carList, HttpStatus.OK);
-        return null;
+        return new ResponseEntity<>(paymentList, HttpStatus.OK);
     }
 
 
