84a1e5fdf7cfeb083a8dbfddb3d4021f07bae1a5
OOOIOOOIO <polite159@gmail.com>
2024-08-10 15:45:24 +0900

    SpringDataWebSupportConfig 추가

---
+++ src/main/java/com/ssg/bidssgket/global/config/SpringDataWebSupportConfig.java
@@ -0,0 +1,9 @@
+package com.ssg.bidssgket.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class SpringDataWebSupportConfig {
+}
