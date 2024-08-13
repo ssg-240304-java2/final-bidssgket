c2115405140afa8f01ad20d5df9ecd86108b0b81
Gyunorld <mingue640@gmail.com>
2024-08-13 16:02:11 +0900

    feat:상품신고

--- src/main/java/com/ssg/bidssgket/user/domain/auction/domain/repository/AuctionRepository.java
+++ src/main/java/com/ssg/bidssgket/user/domain/auction/domain/repository/AuctionRepository.java
@@ -24,7 +24,11 @@ public interface AuctionRepository extends JpaRepository<Auction, Long> {
     int countByMemberNoAndProductNo(Long memberNo, Long productNo);
 
 
-
     List<Auction> findAllByProduct(Product product);
 
+    @Query(value = "SELECT a.* FROM auction a WHERE a.member_no = :memberNo ORDER BY a.tender_date ASC LIMIT 1", nativeQuery = true)
+    List<Auction> findDeleteAuctionByMember(Long memberNo);
+
+    @Query(value = "SELECT a.* FROM auction a WHERE a.product_no = :productNo AND a.tender_deleted = 'false' ORDER BY a.tender_date ASC", nativeQuery = true)
+    List<Auction> findAuctionByProductNo(Long productNo);
 }
