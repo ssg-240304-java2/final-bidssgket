49e8433048b1001cb78ce1ed0e15b34fe78d71f5
finite2030 <finite2030@gmail.com>
2024-08-19 20:30:30 +0900

    feat/payment 비스킷 페이로 상품 주문 로직 코드 리팩토링

--- src/main/java/com/ssg/bidssgket/user/domain/order/application/OrderService.java
+++ src/main/java/com/ssg/bidssgket/user/domain/order/application/OrderService.java
@@ -26,70 +26,49 @@ public class OrderService {
 
     @Transactional
     public void createOrders(Member member, Product product, Payment payment, OrderTransactionType orderTransactionType, DeliveryType deliveryType) {
-        PurchaseOrder purchaseOrder;
-        SaleOrder saleOrder;
+
+        OrderStatus orderStatus = determineOrderStatus(deliveryType);
 
         // 구매 주문서 생성
-        if (deliveryType == DeliveryType.IN_PERSON) {
-
-            // 직거래인 경우 결제 없이 주문서 생성
-            purchaseOrder = PurchaseOrder.addPurchaseOrder(
-                    orderTransactionType,
-                    DeliveryType.IN_PERSON,
-                    OrderStatus.COMPLETED,
-                    member,
-                    product,
-                    null, // 직거래인 경우 결제 정보 없음
-                    null // 직거래는 Parcel 정보 없음
-            );
-        } else if (deliveryType == DeliveryType.ESCROW) {
-
-            // 안전거래인 경우 결제 정보 포함하여 주문서 생성
-            purchaseOrder = PurchaseOrder.addPurchaseOrder(
-                    orderTransactionType,
-                    DeliveryType.ESCROW,
-                    OrderStatus.PAID,
-                    member,
-                    product,
-                    payment,
-                    null // 초기에는 Parcel 정보가 없을 수 있음
-            );
-        } else {
-            throw new IllegalArgumentException("지원되지 않는 배송 유형입니다.");
-        }
+        PurchaseOrder purchaseOrder = PurchaseOrder.addPurchaseOrder(
+                orderTransactionType,
+                deliveryType,
+                orderStatus,
+                member,
+                product,
+                deliveryType == DeliveryType.ESCROW ? payment : null,
+                null // 초기에는 Parcel 정보가 없을 수 있음
+        );
 
         // 구매 주문서 저장
         purchaseOrderRepository.save(purchaseOrder);
 
         // 판매 주문서 생성
-        if (deliveryType == DeliveryType.IN_PERSON) {
-            // 직거래인 경우 결제 없이 판매 주문서 생성
-            saleOrder = SaleOrder.addSaleOrder(
-                    orderTransactionType,
-                    DeliveryType.IN_PERSON,
-                    OrderStatus.COMPLETED,
-                    member,
-                    product,
-                    null, // 직거래인 경우 결제 정보 없음
-                    null // 초기에는 Parcel 정보가 없을 수 있음
-            );
-        } else if (deliveryType == DeliveryType.ESCROW) {
-
-            // 안전거래인 경우 결제 정보 포함하여 판매 주문서 생성
-            saleOrder = SaleOrder.addSaleOrder(
-                    orderTransactionType,
-                    DeliveryType.ESCROW,
-                    OrderStatus.PAID,
-                    member,
-                    product,
-                    payment,
-                    null // 초기에는 Parcel 정보가 없을 수 있음
-            );
-        } else {
-            throw new IllegalArgumentException("지원되지 않는 배송 유형입니다.");
-        }
+        SaleOrder saleOrder = SaleOrder.addSaleOrder(
+                orderTransactionType,
+                deliveryType,
+                orderStatus,
+                member,
+                product,
+                deliveryType == DeliveryType.ESCROW ? payment : null,
+                null // 초기에는 Parcel 정보가 없을 수 있음
+        );
 
         // 판매 주문서 저장
         saleOrderRepository.save(saleOrder);
+    }
+
+    private OrderStatus determineOrderStatus(DeliveryType deliveryType) {
+
+        if (deliveryType == DeliveryType.IN_PERSON) {
+
+            return OrderStatus.COMPLETED;
+        } else if (deliveryType == DeliveryType.ESCROW) {
+
+            return OrderStatus.PAID;
+        } else {
+
+            throw new IllegalArgumentException("지원되지 않는 배송 유형입니다.");
+        }
     }
 }
