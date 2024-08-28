070be67ba76f35952bc9f4a0c8d58a9e30b91d63
kimmingyu <mingue640@gmail.com>
2024-08-28 09:49:23 +0900

    feat: 구매/판매 내역

--- src/main/java/com/ssg/bidssgket/user/domain/payment/application/service/PaymentService.java
+++ src/main/java/com/ssg/bidssgket/user/domain/payment/application/service/PaymentService.java
@@ -24,6 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class PaymentService {
@@ -149,5 +151,9 @@ public class PaymentService {
         productRepository.save(product); // 변경된 상태를 DB에 저장
         System.out.println("product.getProductNo() = " + product.getProductNo());
         System.out.println("product.ss() = " + product.getSalesStatus());
+    }
+
+    public List<Payment> findbyMemberNo(Member member) {
+        return paymentRepository.findByMember(member);
     }
 }
